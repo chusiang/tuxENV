f9fccd0b1ca9cfa6451dbdbaf2fe8f9bd6a0876d
chusiang <chusiang.lai@gmail.com>
2014-10-04 01:19:04 +0800

    removing the print message and writting some explain.

--- home/bin/fouce.py
+++ home/bin/fouce.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+"""
+The script will switch the fouce to the 'process' if it has exist,
+else the script will execute a new 'process'.
+"""
+
 import sys
 import os
 import commands
 
-# 宣告 $1 為 process_name。
+# Get the argument of '$1'.
 process_name = sys.argv[1]
 
 try:
-    print "Switching to the " + process_name + "."
+    # get process list.
     process_output = commands.getoutput('ps -A')
-
     if process_name in process_output:
-        print process_name + " exist."
-
+        # The Caja is different of other process. It usually bas ran at background.
         if process_name == 'caja':
             caja_count = commands.getoutput('xdotool search --onlyvisible caja | wc -l')
             if caja_count > 1:
+                # switch window.
                 os.system("xdotool windowactivate --sync $(xdotool search --onlyvisible " + process_name + " | tail -1) 2> /dev/null")
             else:
+                # execute application.
                 os.system(process_name)
-
         else:
+            # switch window.
             os.system("xdotool windowactivate --sync $(xdotool search --onlyvisible " + process_name + ") 2> /dev/null")
     else:
-        print process_name + " not exist."
+        # execute application.
         os.system(process_name)
-
 except:
-    print "Not running."
+    print "Something wrong, It`s has not run anything."
